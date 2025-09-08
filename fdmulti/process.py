5d57105d6d5fb9f4de5dbab998cb68bbdb5dbb67
matscorse <155652843+matscorse@users.noreply.github.com>
2025-09-08 15:01:25 +0100

    first working version with image clipping and rotation

--- fdmulti/process.py
+++ fdmulti/process.py
@@ -22,12 +22,30 @@ class PROCESS:
     
 
     def clip(self):
-        with Image.open(self.input[0]) as im:
-            this_path = Path(self.input[0])
-            logger.info(f"Original Image  : {im.size}, {this_path.name}, {im.format}, {im.mode}")
-            logger.info(f"Clip Arrangement: {self.arrangement}")
-            clip_dims = (int(im.size[0]/self.arrangement[0]),
-                            int(im.size[1]/self.arrangement[1]))
-            logger.info(f"Clip Dimensions : {clip_dims}")
+        for infile in self.input:
+            with Image.open(infile) as im:
+                this_path = Path(infile).absolute()
+                this_dir = this_path.parent
+                logger.info(f" Output Directory: {this_dir}")
+                logger.info(f" Original Image  : {im.size}, {this_path.name}, {im.format}, {im.mode}")
+                logger.info(f" Clip Arrangement: {self.arrangement}")
+                clip_dims = (int(im.size[0]/self.arrangement[0]),
+                                int(im.size[1]/self.arrangement[1]))
+                logger.info(f" Clip Dimensions : {clip_dims}")
+
+                framecount = 0
+                for y_index in range(self.arrangement[1]):
+                    for x_index in range(self.arrangement[0]):
+                        clip_filename = this_path.stem + f"_{framecount}" + this_path.suffix
+                        logger.info(f" Clipping and creating: {clip_filename}")
+                        cropped = im.crop((x_index * clip_dims[0],
+                                        y_index * clip_dims[1],
+                                        (x_index * clip_dims[0])+clip_dims[0],
+                                        (y_index * clip_dims[1])+clip_dims[1]))
+                        if self.rotation != 0:
+                            cropped = cropped.rotate(float(self.rotation), expand=True)
+                        cropped.save(Path.joinpath(this_dir, clip_filename), quality=100)
+                        framecount += 1
+
 
         return None
